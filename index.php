42464783adc82fb979f944f15a94a8179ebe587e
tuanthanh1011 <92152534+tuanthanh1011@users.noreply.github.com>
2023-11-16 00:16:10 +0700

    Update index.php
    Fix css does not work

--- index.php
+++ index.php
@@ -22,12 +22,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <!-- link css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="../assets/css/base.css">
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <link rel="stylesheet" href="../../assets/css/login.css">
-    <link rel="stylesheet" href="../assets/css/productdetail.css">
-    <link rel="stylesheet" href="../assets/css/reponsive1.css">
-    <link rel="icon" href="../assets/img/logo/main.png" type="image/x-icon" />
+    <link rel="stylesheet" href="assets/css/base.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/productdetail.css">
+    <link rel="stylesheet" href="assets/css/reponsive1.css">
+    <link rel="icon" href="assets/img/logo/main.png" type="image/x-icon" />
     
 </head>
 
